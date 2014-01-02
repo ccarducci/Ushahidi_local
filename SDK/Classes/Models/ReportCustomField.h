7a64014411a3c2fb3d6a348f6a94f4f749e4c2bc
Cristiano Carducci <cristiano.carducci@gmail.com>
2014-01-02 11:17:48 +0100

    no message

---
+++ SDK/Classes/Models/ReportCustomField.h
@@ -0,0 +1,27 @@
+//
+//  ReportCustomField.h
+//  SDK
+//
+//  Created by Cristiano Carducci on 02/01/14.
+//  Copyright (c) 2014 Ushahidi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface ReportCustomField : NSManagedObject
+
+@property (nonatomic, retain) NSString * identifier;
+@property (nonatomic, retain) NSString * name;
+@property (nonatomic, retain) NSNumber * type;
+@property (nonatomic, retain) NSString * defaultvalue;
+@property (nonatomic, retain) NSNumber * required;
+@property (nonatomic, retain) NSNumber * maxlen;
+@property (nonatomic, retain) NSNumber * height;
+@property (nonatomic, retain) NSNumber * isdate;
+@property (nonatomic, retain) NSNumber * ispublicvisible;
+@property (nonatomic, retain) NSNumber * ispublicsubmit;
+@property (nonatomic, retain) NSString * value;
+
+@end
---
+++ SDK/Classes/Models/ReportCustomField.m
@@ -0,0 +1,26 @@
+//
+//  ReportCustomField.m
+//  SDK
+//
+//  Created by Cristiano Carducci on 02/01/14.
+//  Copyright (c) 2014 Ushahidi. All rights reserved.
+//
+
+#import "ReportCustomField.h"
+
+
+@implementation ReportCustomField
+
+@dynamic identifier;
+@dynamic name;
+@dynamic type;
+@dynamic defaultvalue;
+@dynamic required;
+@dynamic maxlen;
+@dynamic height;
+@dynamic isdate;
+@dynamic ispublicvisible;
+@dynamic ispublicsubmit;
+@dynamic value;
+
+@end
