08f66eaa8b43af6e913630bd00b88c4e91a15568
ccarducci <cristiano.carducci@gmail.com>
2014-05-07 22:29:04 +0200

    no message

---
+++ App/Classes/Controllers/USHFieldMultiViewController.h
@@ -0,0 +1,13 @@
+//
+//  USHFieldMultiViewController.h
+//  App
+//
+//  Created by Cristiano Carducci on 06/05/14.
+//  Copyright (c) 2014 Ushahidi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface USHFieldMultiViewController : UITableViewController
+
+@end
