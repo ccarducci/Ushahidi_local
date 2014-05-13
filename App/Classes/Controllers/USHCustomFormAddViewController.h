b98f90daf50a014d1a78c90d5dd93e05dabd8a86
ccarducci <cristiano.carducci@gmail.com>
2014-05-13 23:48:17 +0200

    no message

---
+++ App/Classes/Controllers/USHCustomFormAddViewController.h
@@ -0,0 +1,13 @@
+//
+//  USHCustomFormAddViewController.h
+//  App
+//
+//  Created by Cristiano Carducci on 12/05/14.
+//  Copyright (c) 2014 Ushahidi. All rights reserved.
+//
+
+#import <Ushahidi/Ushahidi.h>
+
+@interface USHCustomFormAddViewController : USHTableViewController
+
+@end
---
+++ App/Classes/Controllers/USHFieldViewController.h
@@ -0,0 +1,13 @@
+//
+//  USHFieldViewController.h
+//  App
+//
+//  Created by Cristiano Carducci on 13/05/14.
+//  Copyright (c) 2014 Ushahidi. All rights reserved.
+//
+
+#import <Ushahidi/Ushahidi.h>
+
+@interface USHFieldViewController : USHTableViewController
+
+@end
