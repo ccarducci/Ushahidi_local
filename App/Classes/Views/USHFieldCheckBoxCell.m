3833ed192804f41be65e89ecda4ecdad7fabab24
ccarducci <cristiano.carducci@gmail.com>
2014-05-14 23:16:07 +0200

    no message

---
+++ App/Classes/Views/USHFieldCheckBoxCell.m
@@ -0,0 +1,31 @@
+//
+//  USHFieldCheckBoxCell.m
+//  App
+//
+//  Created by Cristiano Carducci on 14/05/14.
+//  Copyright (c) 2014 Ushahidi. All rights reserved.
+//
+
+#import "USHFieldCheckBoxCell.h"
+
+@implementation USHFieldCheckBoxCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
